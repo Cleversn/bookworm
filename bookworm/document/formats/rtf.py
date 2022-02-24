ae93540803e5064d57a7bf445dd5ad875a756181
mush42 <musharraf.omer@gmail.com>
2022-02-24 11:47:54 +0200

    WIP: pandoc is choked with large RTF files.

--- bookworm/document/formats/rtf.py
+++ bookworm/document/formats/rtf.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from __future__ import annotations
+import lxml.html
 from bookworm.pandoc import pandoc_convert
 from bookworm.logger import logger
 from .html import BaseHtmlDocument
@@ -32,8 +33,21 @@ class RtfDocument(BaseHtmlDocument):
         return self.parse_to_full_text()
 
     def _get_html_from_rtf(self):
-        return pandoc_convert(
+        raw_html = pandoc_convert(
             self._raw_file_data,
             from_format='rtf',
             to_format='html'
         )
+        html_tree = lxml.html.fromstring(raw_html)
+        if not (html_tree.xpath("/html/head/title//text()")):
+            if not html_tree.xpath('/html/head'):
+                head = html_tree.makeelement('head')
+                html_tree.insert(0, head)
+            if (title_el := html_tree.xpath("/html/head/title")):
+                html_tree.remove(title_el)
+            title = head.makeelement('title')
+            title.text = self.filename.stem
+            html_tree.head.insert(0, title)
+        return lxml.html.tostring(html_tree, encoding='unicode')
+
+
